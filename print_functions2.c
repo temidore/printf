49ad4460c1463ec9139913a7bcb5673614cfb428
Zoe Eriute <zoeerute@gmail.com>
2023-03-29 02:57:33 -0700

    Update print_functions2.c

--- print_functions2.c
+++ print_functions2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/****************** PRINT POINTER ******************/
+/****************** PRINT POINTER ******************/you
 /**
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
